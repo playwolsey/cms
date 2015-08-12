af9abea1cabda8fd7c5c35d5d899ddfb8cdd8d2d
cp <chengpengcp9@163.com>
2015-08-12 10:56:23 +0800

    preview js first commit

---
+++ public/javascripts/tms/preview.js
@@ -0,0 +1,22 @@
+/*
+ * @desc tms preview page render&&event
+ * @author awwwesssooooome <chengpengcp9@gmail.com>
+ * @createTime 2015-08-12
+ */
+
+;(function($, win, undef) {
+    var preview = {
+        init: function() {
+            this.initNode();
+            this.bindEvent();
+        },
+
+        initNode: function() {
+        },
+
+        bindEvent: function() {
+        }
+    };
+
+    preview.init();
+})(jQuery, window);
